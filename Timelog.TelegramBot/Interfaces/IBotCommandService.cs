b08a6c6bb8fe1f3af725412d778c7b6e0abcc8eb
XanCh <xandrmail@mail.ru>
2022-04-19 15:09:28 +0600

    Старт с примера + интерфейс команд

---
+++ Timelog.TelegramBot/Interfaces/IBotCommandService.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot.Types;
+
+namespace Timelog.TelegramBot.Interfaces
+{
+    public interface IBotCommandService 
+    {
+        public void RegisterHandler(string botCommant, Action<Update> action);
+        public void UnregisterHandler(string botCommant);
+        public void UpdateHandler(string botCommant);
+        public void ExecuteCommand(string botCommant, Update update);
+    }
+}
