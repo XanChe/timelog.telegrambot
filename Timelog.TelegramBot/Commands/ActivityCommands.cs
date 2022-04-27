450332dbf517821bff85eb6481c0b18c56e54fcc
XanCh <xandrmail@mail.ru>
2022-04-27 09:45:48 +0600

    новые обработчики комманд

---
+++ Timelog.TelegramBot/Commands/ActivityCommands.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Timelog.Core;
+using Timelog.Core.Services;
+
+namespace Timelog.TelegramBot.Commands
+{
+    public class ActivityCommands
+    {
+        private readonly IUserActivityService _activityService;
+        public ActivityCommands(ITimelogServiceBuilder serviceBuilder)
+        {
+            _activityService = serviceBuilder.CreateUserActivityService();
+
+        }
+        [CommandBind("/activities")]
+        public async Task ReplayActivitiesAsync(ITelegramBotClient botClient, Update update, string parameters)
+        {
+            var activities = await _activityService.GetActivitiesAsync();
+            
+            var message = update.Message;
+            var replayText = new StringBuilder();
+           
+            activities.Aggregate(replayText, (accum, activity) => accum.Append($"* {activity.Comment} \\- {activity.StartTime:t} Длительность: {activity.Duration:hh}:{activity.Duration:mm}\n"));
+            var result = replayText.ToString();
+#nullable disable
+            await botClient.SendTextMessageAsync(message.Chat, result, parseMode: ParseMode.MarkdownV2);
+#nullable enable
+        }
+        
+    }
+}
---
+++ Timelog.TelegramBot/Commands/ChatCommands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Timelog.TelegramBot.Commands
+{
+    public class ChatCommands
+    {
+        [CommandBind("/bind_project")]
+        public async Task BindProjectToCurrentChatCommand(ITelegramBotClient botClient, Update update, string parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
